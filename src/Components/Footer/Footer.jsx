beeb0b0f0f3641b93eb1f67a574b98e7ee6d3192
Nouh1408 <ahmedinouh@gmail.com>
2025-09-22 18:30:28 +0300

    deleted some stuff

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="bg-dark text-light pt-4 mt-5">
   <div className="container">
     <div className="row">
-      {/* Brand / About */}
+      
       <div className="col-md-4 mb-3">
         <h5 className="text-uppercase">MyShop</h5>
         <p>
@@ -14,7 +14,7 @@ export default function Footer() {
           Fast delivery. Easy returns. 100% satisfaction.
         </p>
       </div>
-      {/* Links */}
+      
       <div className="col-md-2 mb-3">
         <h6 className="text-uppercase">Quick Links</h6>
         <ul className="list-unstyled">
@@ -24,7 +24,7 @@ export default function Footer() {
           <li><a href="#" className="text-light text-decoration-none">Contact</a></li>
         </ul>
       </div>
-      {/* Customer Service */}
+      
       <div className="col-md-3 mb-3">
         <h6 className="text-uppercase">Customer Service</h6>
         <ul className="list-unstyled">
@@ -34,7 +34,7 @@ export default function Footer() {
           <li><a href="#" className="text-light text-decoration-none">Terms &amp; Conditions</a></li>
         </ul>
       </div>
-      {/* Newsletter */}
+      
       <div className="col-md-3 mb-3">
         <h6 className="text-uppercase">Stay Connected</h6>
         <form>
@@ -50,7 +50,7 @@ export default function Footer() {
         </div>
       </div>
     </div>
-    {/* Bottom bar */}
+    
     <div className="text-center py-3 border-top">
       <p className="mb-0">© 2025 MyShop. All rights reserved.</p>
     </div>
