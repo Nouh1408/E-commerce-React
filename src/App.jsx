cb36ba68da0c6f2a09ada3a2377a13b20b80781e
Nouh1408 <ahmedinouh@gmail.com>
2025-09-25 21:21:24 +0300

    asdadawd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Components/Layout/Layout.jsx'
 import Home from './Components/Home/Home.jsx'
@@ -6,31 +6,33 @@ import ErrorPath from './Components/eRRORpATH/ErrorPath.jsx'
 import Products from './Components/Products/Products.jsx'
 import Cart from './Components/Cart/Cart.jsx'
 import ItemDescription from './Components/ItemDescription/ItemDescription.jsx'
-import {CartProvider} from './Components/CartContext/CartContext.jsx'
+import { CartProvider } from './Components/CartContext/CartContext.jsx'
 import Category from './Components/Catrgory/Category.jsx'
 
-
 export default function App() {
-  
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          { index: true, element: <Home /> },
+          { path: "/product", element: <Products showPagination={true} /> },
+          { path: "/product/:id", element: <ItemDescription /> },
+          { path: "/cart", element: <Cart /> },
+          { path: "/category/:categoryName", element: <Category /> },
+          { path: "*", element: <ErrorPath /> }
+        ]
+      }
+    ],
+    {
+      basename: "/E-commerce-React", 
+    }
+  );
 
-  const router = createBrowserRouter([
-          {path:"/", element:<Layout/>,children:[
-            {index:true,element:<Home/>},
-            { path: "/product", element: <Products showPagination={true} /> },
-            {path:"/product/:id",element:<ItemDescription/>},
-            {path:"/cart",element:<Cart/>},
-             { path: "/category/:categoryName", element: <Category /> },
-            {path:"*",element:<ErrorPath/>}
-          ]}
-        ])
   return (
-    <div>
-      <CartProvider>
-        <RouterProvider router={router}/>
-        
-      </CartProvider>
-      
-    </div>
-    //now cart and add to cart are available in any place in app
-  )
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  );
 }
